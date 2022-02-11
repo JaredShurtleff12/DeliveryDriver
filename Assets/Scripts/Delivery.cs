1502e5587d37d94529e0b57107f1846ca27216af
Jared Shurtleff <shurtl056@morrisville.edu>
2022-02-10 23:40:02 -0500

    If Statements & Tags

---
+++ Assets/Scripts/Delivery.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Delivery : MonoBehaviour
+{
+    void OnCollisionEnter2D(Collision2D other) 
+    {
+        Debug.Log("Oops, I bumped into a circle!");
+    }
+    void OnTriggerEnter2D(Collider2D other) {
+        // if (the thing we trigger is the package){
+        // then print "package picked up" to the console
+        //  }
+        if (other.tag == "Package"){
+            Debug.Log("Package picked up");
+        }
+        if (other.tag == "Customer"){
+            Debug.Log("Package Delivered!");
+        }
+    }
+}
