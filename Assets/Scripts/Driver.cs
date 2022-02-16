f08e519a637388abef416a793082fa40ec30eba6
Jared Shurtleff <shurtl056@morrisville.edu>
2022-02-16 18:08:42 -0500

    Boosts & Bumps

--- Assets/Scripts/Driver.cs
+++ Assets/Scripts/Driver.cs
@@ -6,9 +6,8 @@ public class Driver : MonoBehaviour
 {
     [SerializeField] float steerSpeed = 0.1f;
     [SerializeField] float moveSpeed = 0.01f;
-    void Start()
-    {
-    }
+    [SerializeField] float slowSpeed = 12f;
+    [SerializeField] float boostSpeed = 25f;
 
     // Update is called once per frame
     void Update()
@@ -18,5 +17,14 @@ public class Driver : MonoBehaviour
          transform.Rotate(0, 0, -steerAmount);
          transform.Translate(0, moveAmount, 0);
 
+    }
+    void OnCollisionEnter(Collision other) {
+        moveSpeed = slowSpeed;
+    }
+    void OnTriggerEnter2D(Collider2D other) {
+        if(other.tag == "Boost"){
+            moveSpeed = boostSpeed;
+        }
+        
     }
 }
